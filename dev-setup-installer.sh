f4858dc8b7dc134ca63c8aff0b42501829bf93e9
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2020-06-23 09:16:02 -0300

    fix(yarn): Fix yarn installation step

--- dev-setup-installer.sh
+++ dev-setup-installer.sh
@@ -6,7 +6,7 @@
 #
 #############################################
 # Default Variables
-  DEFAULT_NODE_VERSION=10.18.0
+  DEFAULT_NODE_VERSION=10.21.0
   DEFAULT_SHELL="zsh"
 #############################################
 # update apt package registry
@@ -58,39 +58,49 @@ source "$HOME"/.zshrc
     source "$HOME"/.nvm/nvm.sh
     [ -n "$(nvm --version)" ] && $(nvm install "$DEFAULT_NODE_VERSION" && nvm use "$DEFAULT_NODE_VERSION")
 
+source "$HOME"/.zshrc
 # get yarn 
-[ -a "$(which node)" ] && ! [ -a "$(which yarn)" ] && curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - \
-    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list  &&
-    apt-get update apt-get install --no-install-recommends yarn
+[ -a "$(which node)" ] && ! [ -a "$(which yarn)" ] && curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add  &&
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list  &&
+    apt-get update && apt-get install --no-install-recommends yarn
 
+source "$HOME"/.zshrc
 # get tmux
 check_package_installed "tmux"
 # setup tmux dotfiles aliases
 ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s -f ~/dotfiles/.tmux.conf.local ~/.tmux.conf.local
 
+source "$HOME"/.zshrc
 # get SilverSeearcher
 check_package_installed "silversearcher-ag"
 
+source "$HOME"/.zshrc
 # get neovim
 check_package_installed "neovim"
 
+source "$HOME"/.zshrc
 # create neovim configuration directory
 [ -d ~/.config ] || mkdir -p ~/.config 
 
+source "$HOME"/.zshrc
 # setup neovim dotfiles aliases
 ln -sf ~/dotfiles/nvim ~/.config/nvim
 
+source "$HOME"/.zshrc
 #get vim-plug
 [ -a "$(which curl)" ] && curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+source "$HOME"/.zshrc
 # install vim plugins
 [ -a "$(which vim)" ] && vim -c 'PlugInstall --sync' + qa 
 
+source "$HOME"/.zshrc
 # get tldr
 [ -a "$(which npm)" ] && npm install -g tldr
 
+source "$HOME"/.zshrc
 # get htop
 check_package_installed "htop"
 
